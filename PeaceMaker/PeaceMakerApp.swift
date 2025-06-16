10ca5f16e34f3cd2ceb3d44b9254f2f2bd06f3e9
rodrigo macias <rodrigoprime@outlook.es>
2025-06-15 19:02:17 -0600

    Initial Commit

---
+++ PeaceMaker/PeaceMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeaceMakerApp.swift
+//  PeaceMaker
+//
+//  Created by Rodrigo Macias Ruiz on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PeaceMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
